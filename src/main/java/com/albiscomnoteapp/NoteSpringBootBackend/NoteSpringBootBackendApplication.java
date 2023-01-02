08dca89d97095924d093deb4313065f8d03d0aeb
unknown <ahmedwoye@gmail.com>
2023-01-02 09:36:36 +0100

    Addng NoteAPP to git repository

---
+++ src/main/java/com/albiscomnoteapp/NoteSpringBootBackend/NoteSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.albiscomnoteapp.NoteSpringBootBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albiscomnoteapp/NoteSpringBootBackend/NoteSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albiscomnoteapp.NoteSpringBootBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
